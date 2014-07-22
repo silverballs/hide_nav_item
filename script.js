080f7bb97c89c62dea08d86388b3016e9cc798c4
Silverballs <asilbersmith11@gmail.com>
2014-07-22 02:09:21 -0400

    Add hideNavElement function as working solutions.  Still need to handle bad input, replace prompt with button and input field, and implement another solution in jquery

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("js file linked");
+// alert("js file linked");
+
+var hideNavElement = function () {
+    var ul = document.getElementById('mainNav');
+    var li = ul.getElementsByTagName('li');
+    console.log(li.length); // make sure you get length
+    var length = (li.length);
+    var n = prompt("What num to last should I remove? ie: 2nd to last removes At");
+    var i = length - n; // subtract n from length to get second to last 
+    li[i].style.display = 'none'; // hide second to last nav item
+};
+
+hideNavElement();
