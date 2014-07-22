08da3acbdf3b2b998706be4747ccab24056e7a92
Silverballs <asilbersmith11@gmail.com>
2014-07-22 02:34:41 -0400

    Noticed mistake in notes - switch a few works that used  second to nth

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var hideNavElement = function () {
     console.log(li.length); // make sure you get length
     var length = (li.length);
     var n = prompt("What num to last should I remove? ie: 2nd to last removes At");
-    var i = length - n; // subtract n from length to get second to last 
-    li[i].style.display = 'none'; // hide second to last nav item
+    var i = length - n; // subtract n from length to get nth to last index to target 
+    li[i].style.display = 'none'; // hide nth to last nav item
 };
 
 hideNavElement();
